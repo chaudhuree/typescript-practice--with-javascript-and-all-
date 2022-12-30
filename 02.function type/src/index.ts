b10bac4472f00a980bf9c47f37659ba056c3d4b5
chaudhuree <chaudhuree00@gmail.com>
2022-12-31 04:42:44 +0600

    docs: default and optional perameter in function

--- 02.function type/src/index.ts
+++ 02.function type/src/index.ts
@@ -1,17 +1,32 @@
-// doc 
+// doc
 // function declaration in typescript
 function intro(name: string, age: number): string {
-    return `My name is ${name} and I am ${age} years old`;
+  return `My name is ${name} and I am ${age} years old`;
 }
 
 // function expression in typescript
-const intro1=function intro(name: string, age: number): string {
-    return `My name is ${name} and I am ${age} years old`;
-}
+const intro1 = function intro(
+  name: string,
+  age: number
+): string {
+  return `My name is ${name} and I am ${age} years old`;
+};
 
 // arrow function
-const intro2=(name: string, age: number): string => {
+const intro2 = (name: string, age: number): string => {
+  return `My name is ${name} and I am ${age} years old`;
+};
+
+// default and optional parameters in typescript
+function intro3(name: string, age: number = 20): string {
+  return `My name is ${name} and I am ${age} years old`;
+}
+
+function intro4(name: string, age?: number): string {
+  if (age) {
     return `My name is ${name} and I am ${age} years old`;
+  }
+  return `My name is ${name}`;
 }
 
 
