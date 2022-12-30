d54491840a65bf7cf6d5133cb4239f79cd43fce8
chaudhuree <chaudhuree00@gmail.com>
2022-12-31 04:36:43 +0600

    docs: function declaration in typescript

--- 02.function type/src/index.ts
+++ 02.function type/src/index.ts
@@ -1,1 +1,17 @@
-// code here
+// doc 
+// function declaration in typescript
+function intro(name: string, age: number): string {
+    return `My name is ${name} and I am ${age} years old`;
+}
+
+// function expression in typescript
+const intro1=function intro(name: string, age: number): string {
+    return `My name is ${name} and I am ${age} years old`;
+}
+
+// arrow function
+const intro2=(name: string, age: number): string => {
+    return `My name is ${name} and I am ${age} years old`;
+}
+
+
